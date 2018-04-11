f2f16ea8a55705dc99d1bc75ad546891d48ea2a9
Freibuis <freibuis@gmail.com>
2018-04-12 09:40:59 +1000

    template confiuration file to load

---
+++ lib/generators/simple_seeds/setup/templates/simple_seeds.rb
@@ -0,0 +1,15 @@
+################################################################
+##
+#  SIMPLE SEEDS CONFIGURATION FILE:
+##
+##
+#
+# This file is allows you to place instance variables or CONSTANTS that persist
+# between Seed files when they load
+#
+# Example:
+#  @max_users = 100
+#   or
+#  MAX_USERS = 100.freeze
+#
+#################################################################
